c8033e6962bb3df49ce5d0be51693cb12dadce45
stijn.verhaegen@student.ap.be <stijn.verhaegen@student.ap.be>
2024-08-18 17:24:12 +0200

    first fetch rbfa api from frontend

--- frontend/typescript/index.ts
+++ frontend/typescript/index.ts
@@ -1,8 +1,10 @@
 import { BACKEND_BASE_URL } from './config.js';
+import { getUpcomingMatch } from './rbfa/api.js';
 
 console.log('Test');
 
 fetch(BACKEND_BASE_URL).then((response) => {
   console.log(response);
 });
-('test2');
+
+console.log(await getUpcomingMatch);
