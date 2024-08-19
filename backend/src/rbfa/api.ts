eb0827bcdabd4b99d7f491c22e2dbc9f19584369
stijn.verhaegen@student.ap.be <stijn.verhaegen@student.ap.be>
2024-08-19 23:22:35 +0200

    backend import path corrected

--- backend/src/rbfa/api.ts
+++ backend/src/rbfa/api.ts
@@ -1,4 +1,4 @@
-import { GetUpcomingMatchAPIResponse } from './types';
+import { GetUpcomingMatchAPIResponse } from './types.js';
 
 export const getUpcomingMatch: Promise<GetUpcomingMatchAPIResponse> = fetch(
   'https://datalake-prod2018.rbfa.be/graphql?operationName=GetUpcomingMatch&variables={"teamId":"307846","language":"nl"}&extensions={"persistedQuery":{"version":1,"sha256Hash":"82e90ddafc6823e8cc5c5d876073e0e01c261f6844ad8e8bab5b8fd0b17da5e1"}}'
