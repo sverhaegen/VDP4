6f967642c88af0fb92307701072f8c67079f8720
stijn.verhaegen@student.ap.be <stijn.verhaegen@student.ap.be>
2024-08-22 21:18:30 +0200

    frontend calls rbfa api directly + extending upcoming match visualization

--- frontend/typescript/rbfa.ts
+++ frontend/typescript/rbfa.ts
@@ -4,7 +4,7 @@ import { GetUpcomingMatchAPIResponse } from './types/rbfa.js';
 // UPCOMING MATCH
 /**********************************************/
 const upcomingMatch: GetUpcomingMatchAPIResponse = await fetch(
-  `${BACKEND_BASE_URL}/upcomingMatch`
+  `https://datalake-prod2018.rbfa.be/graphql?operationName=GetUpcomingMatch&variables={"teamId":"307846","language":"nl"}&extensions={"persistedQuery":{"version":1,"sha256Hash":"82e90ddafc6823e8cc5c5d876073e0e01c261f6844ad8e8bab5b8fd0b17da5e1"}}`
 )
   .then((response) => {
     return response.json();
@@ -14,13 +14,13 @@ const upcomingMatch: GetUpcomingMatchAPIResponse = await fetch(
   });
 
 const homeTeamHTMLElement: HTMLElement | null =
-  document.getElementById('homeTeam');
+  document.getElementById('homeTeamNameID');
 if (homeTeamHTMLElement) {
   homeTeamHTMLElement.innerHTML =
     upcomingMatch.data.upcomingMatch.homeTeam.name;
 }
 const awayTeamHTMLElement: HTMLElement | null =
-  document.getElementById('awayTeam');
+  document.getElementById('awayTeamNameID');
 if (awayTeamHTMLElement) {
   awayTeamHTMLElement.innerHTML =
     upcomingMatch.data.upcomingMatch.awayTeam.name;
@@ -46,4 +46,18 @@ if (startDateHTMLElement) {
     year: 'numeric',
   });
   startDateHTMLElement.innerHTML = startDate;
+}
+const homeTeamImgHTMLElement: HTMLImageElement | null = document.getElementById(
+  'homeTeamImgID'
+) as HTMLImageElement;
+if (homeTeamImgHTMLElement) {
+  let homeTeamImg = upcomingMatch.data.upcomingMatch.homeTeam.logo;
+  homeTeamImgHTMLElement.src = homeTeamImg;
+}
+const awayTeamImgHTMLElement: HTMLImageElement | null = document.getElementById(
+  'awayTeamImgID'
+) as HTMLImageElement;
+if (awayTeamImgHTMLElement) {
+  let awayTeamImg = upcomingMatch.data.upcomingMatch.awayTeam.logo;
+  awayTeamImgHTMLElement.src = awayTeamImg;
 }
